957531ce3301907957a94ac1bcfd71c5ef40daf8
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-05-27 12:23:53 +0530

    Create 14. jQuery and JS Frameworks.js

---
+++ Module-1/JavaScript/14. jQuery and JS Frameworks.js
@@ -0,0 +1,9 @@
+$('#registerBtn').click(() => {
+  console.log("Registered via jQuery");
+});
+
+$('.eventCard').fadeIn();
+$('.eventCard').fadeOut();
+
+// One benefit of using React/Vue:
+// Components re-render dynamically, making UI updates more efficient and scalable.
