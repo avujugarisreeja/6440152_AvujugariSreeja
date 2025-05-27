26b0e3c143536a383d836a37f34f5bf725d15285
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-05-27 13:57:15 +0530

    Create 37. Using javap to Inspect Bytecode.java

---
+++ Module-3/Java/37. Using javap to Inspect Bytecode.java
@@ -0,0 +1,7 @@
+public class BytecodeExample {
+    public static void sayHello() {
+        System.out.println("Hello Bytecode");
+    }
+}
+/*After compiling: javac BytecodeExample.java
+Inspect: javap -c BytecodeExample*/
