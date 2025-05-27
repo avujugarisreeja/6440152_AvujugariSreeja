38b283bf339a2421681ab6031ba339ebfebcbbcc
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-05-27 13:09:06 +0530

    Delete Module-2 directory

--- Module-2/4. Registrations.sql
+++
@@ -1,5 +0,0 @@
-SELECT e.title, COUNT(s.session_id) AS session_count
-FROM Sessions s
-JOIN Events e ON s.event_id = e.event_id
-WHERE TIME(s.start_time) BETWEEN '10:00:00' AND '12:00:00'
-GROUP BY e.event_id;
