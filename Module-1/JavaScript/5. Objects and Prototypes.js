c6328b73e00cd4c55ada41bb9e23d273f2955ebc
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-06-02 09:55:14 +0530

    5. Objects and Prototypes.js

--- Module-1/JavaScript/5. Objects and Prototypes.js
+++ Module-1/JavaScript/5. Objects and Prototypes.js
@@ -10,3 +10,4 @@ Event.prototype.checkAvailability = function () {
 
 const musicEvent = new Event("Jazz Night", "2025-09-15", 20);
 console.log(Object.entries(musicEvent));
+console.log("Available?", musicEvent.checkAvailability());
