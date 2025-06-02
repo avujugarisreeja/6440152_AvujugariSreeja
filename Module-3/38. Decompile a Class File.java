d2ff24dac9f34e64c8af6c86aa8137ce1674c8cd
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-06-02 13:39:03 +0530

    38. Decompile a Class File.java

--- Module-3/38. Decompile a Class File.java
+++ Module-3/38. Decompile a Class File.java
@@ -1,6 +1,11 @@
-Write and compile a simple class (as above)
+Write any Java class, e.g., Test.java
 
-Use a tool like JD-GUI or CFR to open the .class file
+Compile it: javac Test.java
+
+Open Test.class in JD-GUI or use a tool like CFR:
 
 View and analyze the output Java source
+  
+java -jar cfr.jar Test.class
+
 
