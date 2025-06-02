91cd632ff6c44939e2cd7fb723882c1974dc6360
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-06-02 10:37:21 +0530

    10. Modern JavaScript Features.js

--- Module-1/JavaScript/10. Modern JavaScript Features.js
+++ Module-1/JavaScript/10. Modern JavaScript Features.js
@@ -1,15 +1,12 @@
-// Using const for constants
 const register = (name = "Guest") => console.log("Welcome,", name);
 register();
-// Using const for objects
 const event = { name: "Tech Meetup", location: "Hall 1" };
-// Destructuring with const
+// Destructuring 
 const { name, location } = event;
-// Using let for a variable that might change
 let attendeeCount = 0;
 attendeeCount++;
 console.log("Attendee Count:", attendeeCount);
-// Using const and spread operator
 const originalList = ["A", "B"];
+//Spread Operator
 const clonedList = [...originalList, "C"];
 console.log(clonedList);
