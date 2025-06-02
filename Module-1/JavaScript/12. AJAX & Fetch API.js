e12b1feac19fe58c47460841ec4f8cd404ac9001
AVUJUGARI SREEJA <145351071+avujugarisreeja@users.noreply.github.com>
2025-06-02 10:49:16 +0530

    12. AJAX & Fetch API.js

--- Module-1/JavaScript/12. AJAX & Fetch API.js
+++ Module-1/JavaScript/12. AJAX & Fetch API.js
@@ -1,16 +1,18 @@
-fetch("https://mockapi.io/register", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ name: "User", event: "Music Fest" })
-})
-.then(res => res.json())
-.then(data => {
-  console.log("Registered!", data);
-})
-.catch(() => {
-  console.error("Registration failed.");
-});
+function submitData(user) {
+  fetch("https://mockapi.io/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user)
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log("Registered!", data);
+    })
+    .catch(() => {
+      console.error("Registration failed.");
+    });
 
-setTimeout(() => {
-  console.log("Response simulated after delay");
-}, 2000);
+  setTimeout(() => {
+    console.log("Response simulated after delay");
+  }, 2000);
+}
